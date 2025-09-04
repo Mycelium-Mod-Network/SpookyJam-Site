73bd7542071ddd38eaccdc3a5e49493f91de5d67
Tyler Hancock <darklime@live.ca>
2025-09-04 02:52:12 -0600

    Update 404 page.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,26 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				gray: {
-					...colors.neutral,
-				}
-			}
-		},
-	},
-	plugins: [],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            colors: {
+                gray: {
+                    ...colors.neutral,
+                }
+            },
+            animation: {
+                'jumpscare': 'jumpscare 1s cubic-bezier(0, 0, 0.2, 1) forwards',
+            },
+            keyframes: {
+                jumpscare: {
+                    '75%, 100%': {
+                        transform: 'scale(2)',
+                        opacity: '0'
+                    }
+                }
+            }
+        },
+    },
+    plugins: [],
 };
