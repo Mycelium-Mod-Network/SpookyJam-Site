4eb71ceaf873eac167c726a0f23b097730a90e02
Tyler Hancock <darklime@live.ca>
2025-09-07 22:50:51 -0600

    Add base implementation of ideas pages.

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,28 @@
+import slugify from "slugify";
+
 export const TITLE = 'SpookyJam';
 export const DESCRIPTION = 'The official home of the annual SpookyJam mod jam.';
 export const DISCORD = 'https://discord.spooky-jam.com'
 export const GITHUB = 'https://github.com/Mycelium-Mod-Network'
 
 import DATA_SUBMISSIONS from './data/submissions.json'
+
 export const SUBMISSIONS = new Map<string, number[]>(Object.entries(DATA_SUBMISSIONS).sort(([keyA], [keyB]) => Number(keyB) - Number(keyA)))
+
+export interface IdeaEntry {
+    title: string,
+    summary: string,
+    suggester: string,
+    slug?: string
+}
+
+import DATA_IDEAS from './data/ideas.json'
+
+export const IDEAS = new Map<string, IdeaEntry>()
+DATA_IDEAS.ideas.forEach((idea: IdeaEntry) => {
+    idea.slug = slugify(idea.title, {lower: true})
+    if (IDEAS.has(idea.slug)) {
+        throw new Error(`IDea slug already exists. ${idea.slug}`)
+    }
+    IDEAS.set(idea.slug, idea)
+})
