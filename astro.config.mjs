a29d76e60ae9db52714513c58e89e0ab90f4348b
Tyler Hancock <darklime@live.ca>
2026-08-08 18:09:08 -0600

    Migrate to Astro 7 and Tailwind 4.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-import {defineConfig} from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import mdx from "@astrojs/mdx";
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
